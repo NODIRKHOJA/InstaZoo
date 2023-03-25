414a7fb2562308c98708349054b1337457c53a9b
Khoja <figetti@mail.ru>
2023-03-25 10:42:45 +0500

    Post calss

--- src/main/java/com/example/instazoo/entity/Post.java
+++ src/main/java/com/example/instazoo/entity/Post.java
@@ -1,4 +1,26 @@
 package com.example.instazoo.entity;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Post {
+
+    private Long id;
+    private String title;
+    private String cation;
+    private String location;
+    private String likes;
+
+    private Set<String> likedUsers = new HashSet<>();
+    private User user;
+    private List<Comment> comments = new ArrayList<>();
+    private LocalDateTime createdDate;
+
+    protected void onCreate(){
+
+        this.createdDate = LocalDateTime.now();
+    }
 }
